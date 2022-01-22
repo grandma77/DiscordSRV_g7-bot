83646520be515e116511642b41d9d2d099388d99
grandma77 <valterjavornik18@gmail.com>
2022-01-22 13:38:57 +0100

    Part 3

---
+++ commands/info/help.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "help",
+    category: "info",
+    permissions: [],
+    devOnly: false,
+    run: async ({client, message, args}) => {
+        message.reply("This bot is in contstruction mode")
+    }
+}
